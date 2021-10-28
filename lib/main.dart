557ab2b9dbcff61ae1d31986a5c8128f4cd35271
prasad <prasad130495@gmail.com>
2021-10-28 22:59:28 +0800

    Initial UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const LegistApp());
 }
+
 class LegistApp extends StatelessWidget {
   const LegistApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: ' Teczo Legist',
-      home: HomeScreen()
-    );
+    return const MaterialApp(title: ' Teczo Legist', home: HomeScreen());
   }
 }
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -22,16 +22,118 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> _key = GlobalKey(); // Create a key
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0XFF000000),
-        title: Center(child: Text("Teczo Legist"))
-      ),
+    GlobalKey<ScaffoldState> _key = GlobalKey();
+    Color bgColor = Colors.grey.shade200;
 
+    return Scaffold(
+      key: _key,
+      drawer: Drawer(child: Text('test')),
+      appBar: AppBar(
+          leading: GestureDetector(
+            child: Icon(Icons.menu),
+            onTap: () {
+              _key.currentState!.openDrawer();
+            },
+          ),
+          backgroundColor: Color(0XFF000000),
+          title: Center(child: Text("Teczo Legist"))),
+      body: Container(
+        margin: EdgeInsets.all(10),
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                color: bgColor,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded (
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Text(
+                              'Clients',
+                              style: GoogleFonts.openSans(color: Colors.black, fontSize: 30),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Text(
+                              'Total Clients: 2',
+                              style: GoogleFonts.openSans(color: Colors.black, fontSize: 20),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Icon(Icons.person, color: Colors.black,size:100),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: bgColor,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded (
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Text(
+                              'Cases',
+                              style: GoogleFonts.openSans(color: Colors.black, fontSize: 30),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Text(
+                              'Total Cases: 0',
+                              style: GoogleFonts.openSans(color: Colors.black, fontSize: 20),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Icon(Icons.book, color: Colors.black,size:100),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+
+          ],
+        )
+      ),
     );
   }
 }
-
-
